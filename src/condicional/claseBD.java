9693c5068aeab8a46bfe8234baa84b035d9de4c8
inescastillo1968 <inesmayo12345@hotmail.com>
2017-11-10 19:22:50 -0300

    conexion Base de datos

--- src/condicional/claseBD.java
+++ src/condicional/claseBD.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package condicional;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**************************************
  nombre marjorie castillo
@@ -12,6 +15,50 @@ package condicional;
  responsabilidad aplicacion de escritorio
  * @author LAB10
  */
-public class claseBD {
+public class claseBD 
+{
+ 
+    private static Connection conex;//Objeto tipo Connection
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String USER = "root";
+    private static final String PASS = "";
+    private static final String URL = "jdbc:mysql://localhost:3306/claseBD";
     
+    
+    {
+        conex = null;//Inicializar la variable 
+        
+        //Metodo para manejar excepciones
+        try
+        {
+            Class.forName(DRIVER);
+            conex = DriverManager.getConnection(URL, USER, PASS);
+            if (conex != null) 
+            {
+             System.out.println("Conexion Establecida");
+            }
+        }
+        catch(ClassNotFoundException | SQLException e)
+        {
+         System.out.println("Error en la Conexion");
+        }
+    }
+    
+    //PASO 13 - DEFINICION METODOS PARA CONECTAR Y DESCONECTAR
+    public Connection conectar()
+    {
+        return conex;//Retorna el objeto conex  
+    }
+    
+    public void desconectar()
+    {
+      conex=null;
+        if (conex==null) 
+        {
+          System.out.println("Conexion Finalizada");
+        }
+    }
 }
+ 
+
+
--- src/conexionbd2.java
+++ src/conexionbd2.java
@@ -3,24 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pruebajava3;
 
-
-
-/********************************
- *nombre marjorie castillo
+/**********************************
+ * nombre marjorie castillo
  fecha 10-11-17
  clase prueba java 3
  responsabilidad aplicacion de escritorio
  * @author LAB10
  */
-public class PruebaJava3 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class conexionbd2 {
     
 }
